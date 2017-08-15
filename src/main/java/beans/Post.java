e4dff5d6a9cfdd792644d226ca05898b8339e825
R1PLI <treadz11@gmail.com>
2017-08-15 17:35:07 +0300

    api testing: generated pojo and assertion

---
+++ src/main/java/beans/Post.java
@@ -0,0 +1,110 @@
+package beans;
+
+import com.fasterxml.jackson.annotation.*;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "id",
+    "title",
+    "author"
+})
+public class Post {
+
+    @JsonProperty("id")
+    private Long id;
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("author")
+    private String author;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Post() {
+    }
+
+    /**
+     * 
+     * @param author
+     * @param id
+     * @param title
+     */
+    public Post(Long id, String title, String author) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    @JsonProperty("id")
+    public Long getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @JsonProperty("title")
+    public String getTitle() {
+        return title;
+    }
+
+    @JsonProperty("title")
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @JsonProperty("author")
+    public String getAuthor() {
+        return author;
+    }
+
+    @JsonProperty("author")
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(id).append(title).append(author).append(additionalProperties).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Post) == false) {
+            return false;
+        }
+        Post rhs = ((Post) other);
+        return new EqualsBuilder().append(id, rhs.id).append(title, rhs.title).append(author, rhs.author).append(additionalProperties, rhs.additionalProperties).isEquals();
+    }
+
+}
